d2a602fe708bb1f62131efd7d6eb21a2ec0945d8
CyberMansour <ahmedismansour000@gmail.com>
2025-03-22 18:06:55 +0200

    Resolved merge conflict in axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,10 +2,5 @@ import axios from 'axios'
 
 export const axiosInstace = axios.create({
     baseURL: "https://walaaback-production.up.railway.app/api",
-<<<<<<< HEAD
-    withCredentials : true
-})
-=======
     withCredentials: true
 });
->>>>>>> f2ec7f3 (Fixed API base URL and CORS issues)
