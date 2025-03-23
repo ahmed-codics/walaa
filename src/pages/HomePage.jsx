5a6f0fe020b1faf929a9f134360a26076fb45c18
CyberMansour <ahmedismansour000@gmail.com>
2025-03-23 15:50:03 +0200

    Fix: Corrected import path for About.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import About from '../../../walaa/src/components/About'
-import Booking from '../../../walaa/src/components/Booking'
-import ContactUs from '../../../walaa/src/components/ContactUs'
-import Header from '../../../walaa/src/components/Header'
-import HealthPlans from '../../../walaa/src/components/HealthPlans'
-import Modal from '../../../walaa/src/components/Modal'
+import About from '../components/About'
+import Booking from '../components/Booking'
+import ContactUs from '../components/ContactUs'
+import Header from '../components/Header'
+import HealthPlans from '../components/HealthPlans'
+import Modal from '../components/Modal'
 
 
 const HomePage = () => {
