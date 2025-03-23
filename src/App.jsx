59cd63f5591ddcec9104bae27efb5c73f579d663
ahmed-codics <ahmedismansour000@gmail.com>
2025-03-23 19:42:53 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,8 +29,6 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/appointments" element={<ProtectedAppointments />} />
-          <Route path="/booking/:name" element={<Booking />} />
-
         </Routes>
         <ContactUs />
       </ModalProvider>
