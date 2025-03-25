c918e91ce6cc1d2bac6e0be8fd0064026a9700e9
ahmed-codics <ahmedismansour000@gmail.com>
2025-03-25 03:06:10 +0200

    Update useAuthStore.js

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,94 +1,72 @@
- import { generateToken } from "../lib/utils.js";
-import User from "../models/userModel.js";
-import bcrypt from "bcryptjs";
+import {create} from 'zustand'
+import { axiosInstace } from '../lib/axios'
+import toast from  'react-hot-toast'
+import axios from 'axios'
 
-export const signup = async (req, res) => {
-    const { fullName, email, password } = req.body;
-    try {
-      if (!fullName || !email || !password) {
-        return res.status(400).json({ message: "All fields are required" });
-      }
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp : false,
+
+    isLoggingIn : false,
+
+    isCheckingAuth:true,
+
+    checkAuth : async() => {
+        try {
+            const res = await axiosInstace.get("/auth/check");
+
+            set({authUser: res.data})
+        } catch (error) {
+            console.log('error in checkAuth :' , error);
+            
+            set({authUser:null})
+        }  finally {
+            set({isCheckingAuth:false})
+        }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstace.post("/auth/signup", data);
   
-      if (password.length < 6) {
-        return res.status(400).json({ message: "Password must be at least 6 characters" });
-      }
-  
-      const user = await User.findOne({ email });
-  
-      if (user) return res.status(400).json({ message: "Email already exists" });
-  
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-  
-      const newUser = new User({
-        fullName,
-        email,
-        password: hashedPassword,
-      });
-  
-      if (newUser) {
-        // generate jwt token here
-        generateToken(newUser._id, res);
-        await newUser.save();
-  
-        res.status(201).json({
-          _id: newUser._id,
-          fullName: newUser.fullName,
-          email: newUser.email,
-          profilePic: newUser.profilePic,
-        });
-      } else {
-        res.status(400).json({ message: "Invalid user data" });
-      }
-    } catch (error) {
-      console.log("Error in signup controller", error.message);
-      res.status(500).json({ message: "Internal Server Error" });
+    
+            set({ authUser: res.data });
+            toast.success("Account created successfully!");
+            window.location.href = "/"; // Redirect to home page after logout
+
+    
+        } catch (error) {
+            console.log("Signup Error Response:", error.response?.data);
+            toast.error(error.response?.data?.message || "Signup failed!");
+        } finally {
+            set({ isSigningUp: false });
+        }
     }
-  };
-  
-  export const login = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-      const user = await User.findOne({ email });
-  
-      if (!user) {
-        return res.status(400).json({ message: "Invalid credentials" });
-      }
-  
-      const isPasswordCorrect = await bcrypt.compare(password, user.password);
-      if (!isPasswordCorrect) {
-        return res.status(400).json({ message: "Invalid credentials" });
-      }
-  
-      generateToken(user._id, res);
-  
-      res.status(200).json({
-        _id: user._id,
-        fullName: user.fullName,
-        email: user.email,
-        profilePic: user.profilePic,
-      });
-    } catch (error) {
-      console.log("Error in login controller", error.message);
-      res.status(500).json({ message: "Internal Server Error" });
+        ,
+    logout : async () => {
+        try {
+            await axiosInstace.post("/auth/logout");
+            set({authUser : null})
+            toast.success("Logged out successfully!")
+        } catch (error) {
+            toast.error(error.response.data.message)
+
+        }
+    }, 
+
+    login: async (data) => {
+        set({isLoggingIn : true});
+        try {
+            const res = await axiosInstace.post("/auth/login" , data);
+            set({ authUser : res.data });
+            toast.success("Logged in Successfully");
+            window.location.href = "/"; // Redirect to home page after logout
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLoggingIn: false });
+        }
     }
-  };
-  
-  export const logout = (req, res) => {
-    try {
-      res.cookie("jwt", "", { maxAge: 0 });
-      res.status(200).json({ message: "Logged out successfully" });
-    } catch (error) {
-      console.log("Error in logout controller", error.message);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  };
-  
-  export const checkAuth = (req, res) => {
-    try {
-      res.status(200).json(req.user);
-    } catch (error) {
-      console.log("Error in checkAuth controller", error.message);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  };
+
+}))
