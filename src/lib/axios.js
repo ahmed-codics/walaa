6544f549d86a911a3d2eb52d8f539c4956f2be7b
ahmed-codics <ahmedismansour000@gmail.com>
2025-03-25 03:30:06 +0200

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstace = axios.create({
-    baseURL: "walaaback-production-7c7e.up.railway.app/api",
+    baseURL: "https://walaaback-production-7c7e.up.railway.app/api",
     withCredentials: true
 });
