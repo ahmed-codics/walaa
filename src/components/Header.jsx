9f6269dc860b0b4f12b0b104aaaf5cfeb02b7d93
ahmed-codics <ahmedismansour000@gmail.com>
2025-04-10 00:03:52 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,8 +22,8 @@ const Header = () => {
             </span>
           </h1>
 
-  <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-2xl text-blue-700">Hello Habiba! Please click on the Log In button on the right corner.</p>
- <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-2xl text-gray-700"> At Dr. Walaa Gad’s clinic, we are dedicated to providing exceptional physiotherapy services with a focus on personalized care, rehabilitation, and long-term wellness. Our mission is to restore mobility, reduce pain, and enhance the quality of life for every patient through evidence-based techniques and compassionate treatment. Whether you're recovering from injury or seeking preventive care, you're in expert hands.
+  <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-2xl font-montserrat text-blue-700">Hello Habiba! Please click on the Log In button on the right corner.</p>
+ <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-2xl font-montserrat text-gray-700"> At Dr. Walaa Gad’s clinic, we are dedicated to providing exceptional physiotherapy services with a focus on personalized care, rehabilitation, and long-term wellness. Our mission is to restore mobility, reduce pain, and enhance the quality of life for every patient through evidence-based techniques and compassionate treatment. Whether you're recovering from injury or seeking preventive care, you're in expert hands.
 </p>
           {/* Buttons and Badge */}
           <div className="mt-5 flex flex-wrap items-center gap-4">
